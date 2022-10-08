45c344c03c42a7e5356f178675997dc2cfed349c
md-asnawi <muhammadw.2019@scis.smu.edu.sg>
2022-10-08 17:41:39 +0800

    Endpoint returns db record

--- app.js
+++ app.js
@@ -19,17 +19,27 @@ connection.connect(function(err) {
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-})
+});
 
 app.get('/wallet', function (req, res) {
-    connection.query('SELECT * FROM wallet', function (err, res) {
+    connection.query('SELECT * FROM wallet', function (err, result) {
         if (err) {
-            console.log("error");
+            console.log(err);
         } else {
-            console.log(res)
+            res.status(200).json(result[0]);
         }
     })
-})
+});
+
+// app.get('/wallet/:address', function (req, res) {
+//     connection.query('SELECT * FROM wallet', function (err, result) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.status(200).json(result[0]);
+//         }
+//     })
+// });
 
 app.listen(8080, function () {
     console.log('Node app is being served on port: 8080')
--- database.js
+++ database.js
@@ -3,6 +3,7 @@ var mysql = require('mysql2');
 
 var con = mysql.createConnection({
   host: "localhost",
+  database: "MIT",
   user: "root",
   password: process.env.DB_PASSWORD // in .env file, change to password accordingly or empty if needed
 });
